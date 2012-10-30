b07d836465a78901c21c5051c08ddf03c2192bf5
Jasper Spaans <j@jasper.es>
2012-10-30 21:49:10 +0100

    Don't forget the url.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(name='nihilo',
       author='Jasper Spaans',
       author_email='j@jasper.es',
       description='Convenience package offering "nihil"',
+      url='https://github.com/jap/nihilo',
       version='0.1',
       py_modules=['nihilo'],
       )
